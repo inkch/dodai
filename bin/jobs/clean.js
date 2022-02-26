6ab0f652b31055584e97efae837f12ea3de79169
inkch <inkch@posteo.jp>
2022-02-26 19:00:57 +0900

    廃止予定の `rmdirSync` の代わりに `rmSync` を使うよう修正
    実行時に表示された下記のメッセージへの対応。
    
      (node:288563) [DEP0147] DeprecationWarning: In future versions of Node.js,
      fs.rmdir(path, { recursive: true }) will be removed.
      Use fs.rm(path, { recursive: true }) instead

--- bin/jobs/clean.js
+++ bin/jobs/clean.js
@@ -2,5 +2,5 @@ const publicDir = require('../config').path.public
 const fs = require('fs')
 
 module.exports = () => {
-  fs.rmdirSync(publicDir, { recursive: true })
+  fs.rmSync(publicDir, { recursive: true })
 }
